d5bcfcbe74f152dd5e2bdd0eae6670745c56ec38
chi-touch <gloebudav@gmail.com>
2024-10-17 08:45:44 +0100

    first commit

---
+++ src/main/java/devopP2/DevopsP2Application.java
@@ -0,0 +1,13 @@
+package devopP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsP2Application.class, args);
+	}
+
+}
---
+++ src/test/java/devopP2/DevopsP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package devopP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
